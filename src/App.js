8ed7f2051357e69699d805176161d4036000a2f2
Rodrick C <Rodrickmc42@gmail.com>
2022-08-05 15:40:54 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  // an array of components describing countries
+  let [view, setView] = useState([]);
+
+  // intitailizes View array
+  useEffect(() => {
+    setView(old => [])
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id='page-wrapper'>
+      <header>
+        <nav>
+          <h1>Where in the world?</h1>
+          <button>
+            <img src={require('./images/moon-solid.svg').default} alt='moon' /> Dark Mode</button>
+        </nav>
       </header>
+
+      <main>
+        <section className='form-section'>
+          <form action='' className='search-form'>
+            <button type='submit'>
+              <img src={require('./images/search-solid.svg').default} alt='search' />
+            </button>
+            <input type={'search'}
+              id='country-search'
+              name='country-search'
+              placeholder='Search for a country...' />
+          </form>
+          <form action='' className='select-form'>
+            <select name='region' id='region'>
+              <option value="" disabled selected hidden>Filter by Region</option>
+              <option value='Africa'>Africa</option>
+              <option value='America'>America</option>
+              <option value='Asia'>Asia</option>
+              <option value='Europe'>Europe</option>
+              <option value='Oceania'>Oceania</option>
+            </select>
+          </form>
+        </section>
+
+        <section className='countries'>
+
+        </section>
+      </main>
     </div>
   );
 }
